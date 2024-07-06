c8389f8b1f1e06794162f1c038f518a342538dc6
amornthep.c <amornthep.chuen@gmail.com>
2024-07-06 23:26:28 +0700

    setting some config i will go to sleep soon bye

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tsdc-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'TSDC', // Usually your GitHub org/user name.
+  organizationName: 'amornthepc', // Usually your GitHub org/user name.
   projectName: 'tsdc-docs', // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
